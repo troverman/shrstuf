17f0e81f4169925815b9bb4d35a09fe4efebc6cf
RyanJamesAllan <ryanxallan@gmail.com>
2014-09-20 07:48:47 -0400

    view and base chat func

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces')
+var http = require('http').Server(app);
+var cool = require('cool-ascii-faces');
+var io = require('socket.io')(http);
+var $ = require('jquery');
 
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
 
 app.get('/', function(request, response) {
   response.send(cool());
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+
+http.listen(5000, function(){
+  console.log('listening on *:5000');
+});
+
+app.get('/about', function (req, res)
+{
+    res.render('about.html');
+});
+app.get('/chat', function (req, res)
+{
+    res.render('chat.html');
+});
