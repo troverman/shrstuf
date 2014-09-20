c92912a52c4a5a16b075498db9a4f098fd4459e6
Trevor Overman <troverman@gmail.com>
2014-09-20 06:32:39 -0400

    sfs

--- index.js
+++ index.js
@@ -8,15 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.MONGOHQ_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
+
 });
 
 app.get('/', function(request, response) {
