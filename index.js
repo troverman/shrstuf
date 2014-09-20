33d4bae17277e92cd869f5369bfdfa45ebe1df02
Trevor Overman <troverman@gmail.com>
2014-09-20 08:38:55 -0400

    fdfdf

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
