18d7db471814344270081e02943c4af7455f3787
Trevor Overman <troverman@gmail.com>
2014-09-20 06:30:29 -0400

    fdfdfd

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
 var pg = require('pg');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 //mongoose.connect(process.env.MONGOHQ_URL);
-//app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-
+app.get('/db', function (request, response) {
+  pg.connect(process.env.MONGOHQ_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+});
 
 app.get('/', function(request, response) {
   var result = '';
