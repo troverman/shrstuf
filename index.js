0bddb36bb45fb8ecc83999a4a8f785093df064c4
Trevor Overman <troverman@gmail.com>
2014-09-20 03:17:51 -0400

    testing

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 var express = require('express')
 var app = express();
 var cool = require('cool-ascii-faces')
+var pg = require('pg');
 
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
+
 app.get('/', function(request, response) {
-  response.send(cool());
+  var result = ''
+  var times = process.env.TIMES || 100
+  for (i=0; i < times; i++)
+    result += cool();
+  response.send(result);
 });
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
