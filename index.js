135394c79d94c313ecbc09644c963426132b7e94
RyanJamesAllan <ryanxallan@gmail.com>
2014-09-21 05:32:50 -0400

    hiiiii

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var ejs = require('ejs');
 var cool = require('cool-ascii-faces');
 var io = require('socket.io')(http);
 var $ = require("jquery");
