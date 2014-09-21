acd4b382946451d7543a11d0bafae8de2051732d
RyanJamesAllan <ryanxallan@gmail.com>
2014-09-21 07:27:50 -0400

    fuck

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(app.get("port"), function(){
+server.listen(app.get("port"), function(){
   console.log('listening on *:5000');
 });
 
