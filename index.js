70ad58972d30aa8166b1e6e8e51e3e736c08399d
RyanJamesAllan <ryanxallan@gmail.com>
2014-09-21 06:05:16 -0400

    hiiiii

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var $ = require("jquery");
 var passport = require('passport');
 var server = http.createServer(function(req, res) {
     var dirPath = __dirname + '/views';
-    var template = res.send(data.toString("utf8"));
+    var template = res.send(dirPath.toString("utf8"));
     res.end(ejs.render(template,{}));
 });
 
