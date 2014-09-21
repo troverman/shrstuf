07563cab7b2fb2542607937f3d62cb51d0354a68
RyanJamesAllan <ryanxallan@gmail.com>
2014-09-21 05:19:44 -0400

    hiiiii

---
+++ app/routes.js
@@ -0,0 +1,64 @@
+module.exports = function(app, passport) {
+
+
+app.get('/', function(req, res) {
+    res.render("index");
+});
+
+app.get('/about', function (req, res)
+{
+    res.render("about");
+});
+
+app.get('/account', function (req, res)
+{
+    res.render('account');
+});
+
+app.get('/api', function (req, res)
+{
+    res.render('api');
+});
+
+app.get('/chat', function (req, res)
+{
+    res.render('chat');
+});
+
+app.get('/discover', function (req, res)
+{
+    res.render('discover');
+});
+
+app.get('/member', function (req, res)
+{
+    res.render('member');
+});
+
+app.get('/project', function (req, res){
+    res.render('project');
+});
+
+app.get('/search', function (req, res){
+    res.render('search');
+});
+
+
+
+	// process the signup form
+app.post('/signup', passport.authenticate('local-signup', {
+	successRedirect : '/profile', // redirect to the secure profile section
+	failureRedirect : '/signup', // redirect back to the signup page if there is an error
+	failureFlash : true // allow flash messages
+}));
+};
+
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on 
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
