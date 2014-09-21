40398c3ba81c7e661f313cc106dc1638677a57e7
RyanJamesAllan <ryanxallan@gmail.com>
2014-09-21 05:22:02 -0400

    hiiiii

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ paypal_api.payment.create(create_payment_json, config_opts, function (err, res) 
     }
 });
 
-require('./app/routes.js')(passport);
+require('./routes.js')(app, passport);
 require('./config/passport')(passport);
 
 io.on('connection', function(socket){
