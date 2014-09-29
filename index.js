3694a68dcb4f8b8e4273e449281bb730b3c7e176
Trevor Overman <troverman@gmail.com>
2014-09-29 15:29:06 -0400

    re

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /*
 set up addons
 */
-
+require('./app/models/models.js')
 var express = require('express');
 var app = express();
 var http = require('http');
 var passport = require('passport');
 var cool = require('cool-ascii-faces');
 var io = require('socket.io')(http);
-var $ = require("jquery");
+var $ = require('jquery');
 var server = http.createServer(app);
 
 app.use('/static', express.static(__dirname + '/static'));
@@ -16,11 +16,30 @@ app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOHQ_URL);
-var db = mongoose.connection;
+app.use( express.json());
+app.use( express.urlencoded());
+app.use( express.methodOverride());
+
+
+
+
+
 
 require('./app/routes.js')(app, passport);
+
+
+exports.index = function ( req, res ){
+  res.render( 'index', { title : 'Express Todo Example' });
+};
+
+exports.index = function ( req, res ){
+  Todo.find( function ( err, todos, count ){
+    res.render( 'index', {
+      title : 'Express Todo Example',
+      todos : todos
+    });
+  });
+};
 
 //app.get('/test', function (req, res){
 //    mongoose.model('member').find(function(err, member)){
