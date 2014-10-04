79e025f5977584f7d2962346a1baafc298ec5174
Trevor Overman <troverman@gmail.com>
2014-10-04 17:32:01 -0400

    fd

--- controllers/about.js
+++ controllers/about.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/about', function (req, res){
+router.get('/', function (req, res){
     res.render('about');
 });
 
